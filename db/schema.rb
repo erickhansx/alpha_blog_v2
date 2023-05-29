67c9973d59730ae3ad1a7b84e53360b03329409e
erickhansx <erickhansx@gmail.com>
2023-05-29 17:00:03 -0600

    Add category, test and integration test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_010147) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_193212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_010147) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
